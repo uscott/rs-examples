964612081e552ac73108bdacc3d9a562782c0988
US <uriel.scott@protonmail.com>
2020-12-19 05:01:26 -0600

    Remove a bunch of build files

--- clippy/target/rls/debug/deps/clippy2-b49a80f896964c30.d
+++
@@ -1,5 +0,0 @@
-/Users/swamp/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-b49a80f896964c30.rmeta: clippy2.rs
-
-/Users/swamp/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-b49a80f896964c30.d: clippy2.rs
-
-clippy2.rs:
--- clippy/target/rls/debug/deps/clippy2-fc8e40b9befce53c.d
+++
@@ -1,5 +0,0 @@
-/Users/swamp/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-fc8e40b9befce53c.rmeta: clippy2.rs
-
-/Users/swamp/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-fc8e40b9befce53c.d: clippy2.rs
-
-clippy2.rs:
